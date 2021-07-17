4f44b9ec8605e7be4806074ffead2673f4d350a7
skat <fishmoders@petruchio.org.ru>
2021-07-17 19:08:24 +0300

    Records, jdk16 support

---
+++ src/main/java/com/github/bsideup/jabel/Desugar.java
@@ -0,0 +1,10 @@
+package com.github.bsideup.jabel;
+
+import java.lang.annotation.*;
+
+@Documented
+@Retention(RetentionPolicy.SOURCE)
+@Target(ElementType.TYPE)
+public @interface Desugar {
+
+}
--- src/main/java/com/github/bsideup/jabel/JabelCompilerPlugin.java
+++ src/main/java/com/github/bsideup/jabel/JabelCompilerPlugin.java
@@ -2,86 +2,107 @@ package com.github.bsideup.jabel;
 
 import com.sun.source.util.JavacTask;
 import com.sun.source.util.Plugin;
+import com.sun.tools.javac.api.JavacTaskImpl;
 import com.sun.tools.javac.code.Source;
-import com.sun.tools.javac.parser.JavaTokenizer;
-import com.sun.tools.javac.parser.JavacParser;
+import com.sun.tools.javac.util.Context;
+import com.sun.tools.javac.util.JavacMessages;
 import net.bytebuddy.ByteBuddy;
 import net.bytebuddy.agent.ByteBuddyAgent;
 import net.bytebuddy.asm.Advice;
+import net.bytebuddy.asm.AsmVisitorWrapper;
+import net.bytebuddy.asm.MemberSubstitution;
+import net.bytebuddy.dynamic.ClassFileLocator;
+import net.bytebuddy.dynamic.loading.ClassInjector;
 import net.bytebuddy.dynamic.loading.ClassReloadingStrategy;
+import net.bytebuddy.implementation.bytecode.Removal;
+import net.bytebuddy.implementation.bytecode.StackManipulation;
+import net.bytebuddy.implementation.bytecode.constant.IntegerConstant;
+import net.bytebuddy.pool.TypePool;
+import net.bytebuddy.utility.JavaModule;
 
-import java.lang.reflect.Field;
-import java.util.Arrays;
-import java.util.Objects;
-import java.util.Set;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import java.util.*;
 
-import static net.bytebuddy.matcher.ElementMatchers.named;
-import static net.bytebuddy.matcher.ElementMatchers.takesArguments;
+import static net.bytebuddy.matcher.ElementMatchers.*;
 
 public class JabelCompilerPlugin implements Plugin {
 
-    static final Set<Source.Feature> ENABLED_FEATURES = Stream
-            .of(
-                    "PRIVATE_SAFE_VARARGS",
+    static {
+        Map<String, AsmVisitorWrapper> visitors = new HashMap<String, AsmVisitorWrapper>() {{
+            // Disable the preview feature check
+            AsmVisitorWrapper checkSourceLevelAdvice = Advice.to(CheckSourceLevelAdvice.class)
+                    .on(named("checkSourceLevel").and(takesArguments(2)));
 
-                    "SWITCH_EXPRESSION",
-                    "SWITCH_RULE",
-                    "SWITCH_MULTIPLE_CASE_LABELS",
+            // Allow features that were introduced together with Records (local enums, static inner members, ...)
+            AsmVisitorWrapper allowRecordsEraFeaturesAdvice = MemberSubstitution.relaxed()
+                    .field(named("allowRecords"))
+                    .onRead()
+                    .replaceWith((instrumentedType, instrumentedMethod, typePool) ->
+                            (targetType, target, parameters, result, freeOffset) ->
+                                    new StackManipulation.Compound(Removal.of(targetType), IntegerConstant.forValue(true)))
+                                    // remove aload_0
+                    .on(any());
 
-                    "LOCAL_VARIABLE_TYPE_INFERENCE",
-                    "VAR_SYNTAX_IMPLICIT_LAMBDAS",
+            put("com.sun.tools.javac.parser.JavacParser", new AsmVisitorWrapper.Compound(
+                    checkSourceLevelAdvice, allowRecordsEraFeaturesAdvice));
 
-                    "DIAMOND_WITH_ANONYMOUS_CLASS_CREATION",
+            put("com.sun.tools.javac.parser.JavaTokenizer", checkSourceLevelAdvice);
 
-                    "EFFECTIVELY_FINAL_VARIABLES_IN_TRY_WITH_RESOURCES",
+            put("com.sun.tools.javac.comp.Check", allowRecordsEraFeaturesAdvice);
+            put("com.sun.tools.javac.comp.Attr", allowRecordsEraFeaturesAdvice);
+            put("com.sun.tools.javac.comp.Resolve", allowRecordsEraFeaturesAdvice);
 
-                    "TEXT_BLOCKS",
+            // Lower the source requirement for supported features
+            put("com.sun.tools.javac.code.Source$Feature", Advice.to(AllowedInSourceAdvice.class)
+                    .on(named("allowedInSource").and(takesArguments(1))));
+        }};
 
-                    "PATTERN_MATCHING_IN_INSTANCEOF",
-                    "REIFIABLE_TYPES_INSTANCEOF"
-            )
-            .map(name -> {
-                try {
-                    return Source.Feature.valueOf(name);
-                } catch (IllegalArgumentException e) {
-                    return null;
-                }
-            })
-            .filter(Objects::nonNull)
-            .collect(Collectors.toSet());
-
-    @Override
-    public void init(JavacTask task, String... args) {
-        ByteBuddyAgent.install();
+        try {
+            ByteBuddyAgent.install();
+        } catch (Exception e) {
+            ByteBuddyAgent.install(new ByteBuddyAgent.AttachmentProvider.Compound(
+                    ByteBuddyAgent.AttachmentProvider.ForJ9Vm.INSTANCE,
+                    ByteBuddyAgent.AttachmentProvider.ForStandardToolsJarVm.JVM_ROOT,
+                    ByteBuddyAgent.AttachmentProvider.ForStandardToolsJarVm.JDK_ROOT,
+                    ByteBuddyAgent.AttachmentProvider.ForStandardToolsJarVm.MACINTOSH,
+                    ByteBuddyAgent.AttachmentProvider.ForUserDefinedToolsJar.INSTANCE,
+                    ByteBuddyAgent.AttachmentProvider.ForEmulatedAttachment.INSTANCE
+            ));
+        }
 
         ByteBuddy byteBuddy = new ByteBuddy();
 
-        for (Class<?> clazz : Arrays.asList(JavacParser.class, JavaTokenizer.class)) {
-            byteBuddy
-                    .redefine(clazz)
-                    .visit(
-                            Advice.to(CheckSourceLevelAdvice.class)
-                                    .on(named("checkSourceLevel").and(takesArguments(2)))
-                    )
-                    .make()
-                    .load(clazz.getClassLoader(), ClassReloadingStrategy.fromInstalledAgent());
-        }
+        ClassLoader classLoader = JavacTask.class.getClassLoader();
+        ClassFileLocator classFileLocator = ClassFileLocator.ForClassLoader.of(classLoader);
+        TypePool typePool = TypePool.ClassLoading.of(classLoader);
 
-        try {
-            Field field = Source.Feature.class.getDeclaredField("minLevel");
-            field.setAccessible(true);
+        visitors.forEach((className, visitor) -> byteBuddy.redefine(typePool.describe(className).resolve(), classFileLocator)
+                .visit(visitor)
+                .make()
+                .load(classLoader, ClassReloadingStrategy.fromInstalledAgent()));
 
-            for (Source.Feature feature : ENABLED_FEATURES) {
-                field.set(feature, Source.JDK8);
-                if (!feature.allowedInSource(Source.JDK8)) {
-                    throw new IllegalStateException(feature.name() + " minLevel instrumentation failed!");
-                }
-            }
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+        JavaModule jabelModule = JavaModule.ofType(JabelCompilerPlugin.class);
+        ClassInjector.UsingInstrumentation.redefineModule(
+                ByteBuddyAgent.getInstrumentation(),
+                JavaModule.ofType(JavacTask.class),
+                Collections.emptySet(),
+                Collections.emptyMap(),
+                new HashMap<String, Set<JavaModule>>() {{
+                    put("com.sun.tools.javac.api", Collections.singleton(jabelModule));
+                    put("com.sun.tools.javac.tree", Collections.singleton(jabelModule));
+                    put("com.sun.tools.javac.code", Collections.singleton(jabelModule));
+                    put("com.sun.tools.javac.util", Collections.singleton(jabelModule));
+                }},
+                Collections.emptySet(),
+                Collections.emptyMap()
+        );
+    }
+
+    @Override
+    public void init(JavacTask task, String... args) {
+        Context context = ((JavacTaskImpl) task).getContext();
+        JavacMessages.instance(context).add(locale -> new DesugarResourceBundle());
+
+        task.addTaskListener(new RecordsRetrofittingTaskListener(context));
     }
 
     @Override
@@ -92,5 +113,53 @@ public class JabelCompilerPlugin implements Plugin {
     // Make it auto start on Java 14+
     public boolean autoStart() {
         return true;
+    }
+
+    static class AllowedInSourceAdvice {
+
+        @Advice.OnMethodEnter
+        static void allowedInSource(@Advice.This Source.Feature feature,
+                                    @Advice.Argument(value = 0, readOnly = false) Source source) {
+            switch (feature.name()) {
+                case "PRIVATE_SAFE_VARARGS":
+                case "SWITCH_EXPRESSION":
+                case "SWITCH_RULE":
+                case "SWITCH_MULTIPLE_CASE_LABELS":
+                case "LOCAL_VARIABLE_TYPE_INFERENCE":
+                case "VAR_SYNTAX_IMPLICIT_LAMBDAS":
+                case "DIAMOND_WITH_ANONYMOUS_CLASS_CREATION":
+                case "EFFECTIVELY_FINAL_VARIABLES_IN_TRY_WITH_RESOURCES":
+                case "TEXT_BLOCKS":
+                case "PATTERN_MATCHING_IN_INSTANCEOF":
+                case "REIFIABLE_TYPES_INSTANCEOF":
+                case "RECORDS":
+                    //noinspection UnusedAssignment
+                    source = Source.DEFAULT;
+                    break;
+            }
+        }
+    }
+
+    static class CheckSourceLevelAdvice {
+
+        @Advice.OnMethodEnter
+        static void checkSourceLevel(@Advice.Argument(value = 1, readOnly = false) Source.Feature feature) {
+            if (feature.allowedInSource(Source.JDK8)) {
+                //noinspection UnusedAssignment
+                feature = Source.Feature.LAMBDA;
+            }
+        }
+    }
+
+    private static class DesugarResourceBundle extends ResourceBundle{
+        @Override
+        protected Object handleGetObject(String key) {
+            return "{0}";
+        }
+
+        @Override
+        public Enumeration<String> getKeys() {
+            return Collections.enumeration(Collections.singletonList("missing.desugar.on.record"));
+        }
     }
 }
